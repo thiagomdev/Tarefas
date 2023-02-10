b49d53671504ae8478e2f68d0c9358d075553853
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2023-02-10 20:01:41 -0300

    Editing files

--- TODO/MainViewController.swift
+++ TODO/MainViewController.swift
@@ -13,6 +13,7 @@ final class MainViewController: UIViewController {
         table.register(MainTableViewCell.self, forCellReuseIdentifier: MainTableViewCell.identifier)
         table.dataSource = self
         table.delegate = self
+        table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
     
@@ -87,7 +88,7 @@ extension MainViewController: ViewConfiguration {
     }
     
     func setupConstraints() {
-        NSLayoutConstraint.turnOn([
+        NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
